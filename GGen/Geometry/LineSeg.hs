61fe36f1f2fdf75d1a2a671de14da98a01ac4f90
Ben Gamari <bgamari.foss@gmail.com>
2011-10-04 13:10:38 -0400

    Work on QuickCheck tests

--- GGen/Geometry/LineSeg.hs
+++ GGen/Geometry/LineSeg.hs
@@ -1,14 +1,20 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module GGen.Geometry.LineSeg ( invertLineSeg
                              , lineSegDispl
                              , mergeLineSegs
                              , mergeLineSegs'
+                             , runTests
                              ) where
 
 import Data.List ((\\), foldl')
 import Data.Maybe (mapMaybe, isJust, fromJust)
 import Data.VectorSpace
-import Data.VectorSpace.QuickCheck
 import GGen.Geometry.Types
+
+import Test.QuickCheck.All
+import Test.QuickCheck.Property
+import Data.VectorSpace.QuickCheck
 
 -- | Reverse the order of line segment termini
 invertLineSeg :: LineSeg -> LineSeg
@@ -28,10 +34,10 @@ tryMergeLineSegs a b =
             f (LineSeg a1 a2, LineSeg b1 b2) =
                     if a1 `samePoint` b1 then Just $ LineSeg a2 b2
                                          else Nothing
-            dirDev = abs (lineSegDispl a <.> lineSegDispl b) - 1
             merged = mapMaybe f perms
-        in if dirDev < dirTol && (not $ null merged) then Just $ head merged
-                                                     else Nothing
+            parallel = sameDir (lineSegDispl a) (lineSegDispl b)
+        in if parallel && (not $ null merged) then Just $ head merged
+                                              else Nothing
 
 -- | Merge two line segments if possible, otherwise return both segments
 mergeLineSegs :: LineSeg -> LineSeg -> [LineSeg]
@@ -56,13 +62,20 @@ prop_invert_displacement :: LineSeg -> Bool
 prop_invert_displacement l = (lineSegDispl $ invertLineSeg l) == (negateV $ lineSegDispl l)
 
 -- | Split a line segment in two and ensure that the pieces are merged
-prop_merge_divided :: LineSeg -> Bool
-prop_merge_divided l = isJust m && l == fromJust m
-                       where f = lerp (lsBegin l) (lsEnd l)
-                             a = LineSeg (f 0) (f 0.5)
-                             b = LineSeg (f 0.5) (f 1)
-                             m = tryMergeLineSegs a b
+prop_merge_divided :: LineSeg -> Bool -> Bool -> Result
+prop_merge_divided l flipA flipB | magnitude (lineSegDispl l) == 0   = rejected
+prop_merge_divided l flipA flipB =
+        case m of
+             Just l    -> if passed then succeeded
+                                    else failed {reason="Deviation too large"}
+             Nothing   -> failed {reason="Not merged"}
+        where f = lerp (lsBegin l) (lsEnd l)
+              a = (if flipA then invertLineSeg else id) $ LineSeg (f 0) (f 0.5)
+              b = (if flipB then invertLineSeg else id) $ LineSeg (f 0.5) (f 1)
+              m = tryMergeLineSegs a b
+              diffBegin = magnitude $ lsBegin l ^-^ lsBegin (fromJust m)
+              diffEnd   = magnitude $ lsEnd l ^-^ lsEnd (fromJust m)
+              passed = diffBegin < 1e-5 && diffEnd < 1e-5
 
-props = [ prop_invert_displacement
-        , prop_merge_divided
-        ]
+runTests = $quickCheckAll
+
