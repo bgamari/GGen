e40d8fbb05d3967cefb669e30caac7da31ee853c
Ben Gamari <bgamari.foss@gmail.com>
2011-10-04 13:33:12 -0400

    Take interpolation parameter as parameter in prop_merge_divided

--- GGen/Geometry/LineSeg.hs
+++ GGen/Geometry/LineSeg.hs
@@ -62,21 +62,22 @@ prop_invert_displacement :: LineSeg -> Bool
 prop_invert_displacement l = (lineSegDispl $ invertLineSeg l) == (negateV $ lineSegDispl l)
 
 -- | Split a line segment in two and ensure that the pieces are merged
-prop_merge_divided :: LineSeg -> Bool -> Bool -> Result
-prop_merge_divided l flipA flipB
+prop_merge_divided :: LineSeg -> Normalized Double -> Bool -> Bool -> Result
+prop_merge_divided l (Normalized s) flipA flipB
         | magnitude (lineSegDispl l) == 0   = rejected
         | otherwise = case m of
                            Just l    -> if passed then succeeded
                                                   else failed {reason="Deviation too large"}
                            Nothing   -> failed {reason="Not merged"}
                       where f = lerp (lsBegin l) (lsEnd l)
-                            a = (if flipA then invertLineSeg else id) $ LineSeg (f 0) (f 0.5)
-                            b = (if flipB then invertLineSeg else id) $ LineSeg (f 0.5) (f 1)
+                            a = (if flipA then invertLineSeg else id) $ LineSeg (f 0) (f s)
+                            b = (if flipB then invertLineSeg else id) $ LineSeg (f s) (f 1)
                             m = tryMergeLineSegs a b
                             diffBegin = magnitude $ lsBegin l ^-^ lsBegin (fromJust m)
                             diffEnd   = magnitude $ lsEnd l ^-^ lsEnd (fromJust m)
                             passed = diffBegin < 1e-5 && diffEnd < 1e-5
 
+-- | Make sure segments which aren't parallel aren't merged
 prop_dont_merge_nonparallel :: LineSeg -> LineSeg -> Result
 prop_dont_merge_nonparallel a b
         | abs (normalized (lineSegDispl a) <.> normalized (lineSegDispl b) - 1) == 0    = rejected
