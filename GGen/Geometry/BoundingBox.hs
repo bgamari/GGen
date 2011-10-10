fd8e8d948bd439cc9d27a4d3793d1aa3e49b6f26
Ben Gamari <bgamari.foss@gmail.com>
2011-10-10 12:51:36 -0400

    Add more bounding box functions

--- GGen/Geometry/BoundingBox.hs
+++ GGen/Geometry/BoundingBox.hs
@@ -1,13 +1,29 @@
-module GGen.Geometry.BoundingBox ( facesBoundingBox
+module GGen.Geometry.BoundingBox ( points2BoundingBox
+                                 , pointsBoundingBox
+                                 , facesBoundingBox
+                                 , polygons2BoundingBox
                                  ) where
 
 import GGen.Geometry.Types
 
+points2BoundingBox :: [Point2] -> Box Point2
+points2BoundingBox points =
+        let (xs,ys) = unzip points
+        in ( (minimum xs, minimum ys)
+           , (maximum xs, maximum ys) )
+
+pointsBoundingBox :: [Point] -> Box Point
+pointsBoundingBox points =
+        let (xs,ys,zs) = unzip3 points
+        in ( (minimum xs, minimum ys, minimum zs)
+           , (maximum xs, maximum ys, maximum zs) )
+
 facesBoundingBox :: [Face] -> Box Point
-facesBoundingBox faces = let getAllVerts face = let (a,b,c) = faceVertices face
-                                                in [a,b,c]
-                             vs = concat $ map getAllVerts faces
-                             (xs,ys,zs) = unzip3 vs
-                  in ( (minimum xs, minimum ys, minimum zs)
-                     , (maximum xs, maximum ys, maximum zs) )
+facesBoundingBox faces =
+        let getAllVerts face = let (a,b,c) = faceVertices face
+                               in [a,b,c]
+        in pointsBoundingBox $ concat $ map getAllVerts faces
+
+polygons2BoundingBox :: [Polygon Point2] -> Box Point2
+polygons2BoundingBox polys = points2BoundingBox $ concat $ map (\(Polygon points) -> points) polys
 
