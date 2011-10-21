d07aefcd41b657dca4b020057d05881eb6158a64
Ben Gamari <bgamari.foss@gmail.com>
2011-10-21 16:42:24 -0400

    Add GGen.Geometry.Face
    Currently just contains function for moving face vertices

--- GGen/Geometry.hs
+++ GGen/Geometry.hs
@@ -2,10 +2,12 @@ module GGen.Geometry ( module GGen.Geometry.Types
                      , module GGen.Geometry.LineSeg
                      , module GGen.Geometry.Intersect
                      , module GGen.Geometry.BoundingBox
+                     , module GGen.Geometry.Face
                      ) where
 
 import GGen.Geometry.Types hiding (runTests)
 import GGen.Geometry.LineSeg hiding (runTests)
 import GGen.Geometry.Intersect hiding (runTests)
 import GGen.Geometry.BoundingBox
+import GGen.Geometry.Face
 
---
+++ GGen/Geometry/Face.hs
@@ -0,0 +1,13 @@
+module GGen.Geometry.Face ( moveVertex
+                          ) where
+
+import Data.VectorSpace
+import GGen.Geometry.Types
+
+-- | If the given point v is a vertex of the face, move it to v'. Otherwise
+-- return the original face.
+moveVertex :: Face -> Point -> Point -> Face
+moveVertex (Face n (a,b,c)) v v' = let a' = if a =~ v then v' else a
+                                       b' = if b =~ v then v' else b
+                                       c' = if c =~ v then v' else c
+                                   in Face n (a',b',c')
