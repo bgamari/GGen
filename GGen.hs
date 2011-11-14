113943fe77436289e373bd873e077bad91e934a0
Ben Gamari <bgamari.foss@gmail.com>
2011-11-13 20:55:33 -0500

    Split configuration from logic

---
+++ GGen.hs
@@ -0,0 +1,94 @@
+{-# LANGUAGE TupleSections, PackageImports #-}
+
+module GGen( GGenSettings(..)
+           , slice
+           , ggenMain
+           -- For convenience
+           , hexInfill
+           , comment
+           , GCodeSettings(..)
+           ) where
+
+import Data.VectorSpace
+import Data.AffineSpace
+import Data.Maybe (mapMaybe, maybe)
+import Data.Either (either, partitionEithers)
+import Data.List (isSuffixOf, deleteFirstsBy)
+import Text.Printf
+import System.IO
+import System.Environment (getArgs)
+import "mtl" Control.Monad.State (evalState)
+
+import Data.STL
+import GGen.Geometry
+import GGen.Geometry.Polygon
+import qualified GGen.Pretty as P
+import GGen.Render
+import Text.PrettyPrint.HughesPJ (($$), (<+>))
+import GGen.Types
+import GGen.ToolPath
+import GGen.GCode
+
+import Graphics.Rendering.Cairo
+import Control.Monad (liftM)
+
+data GGenSettings s = GGenSettings { ggSliceZStep :: Double
+                                   , ggInfillPattern :: InfillPattern s
+                                   , ggGCodeSettings :: GCodeSettings
+                                   } deriving (Show, Eq)
+
+-- | When we slice exactly in the plane of a face, nasty things can happen with
+-- numerical error. While we try to handle these properly, sliceFudge is one
+-- final line of defence. We shift all of the slices by this amount to ensure
+-- that we avoid these problems, even with nice even sliceZSteps.
+sliceFudge = 1e-6
+
+ggenMain :: GGenSettings s -> IO ()
+ggenMain settings =
+        do filename:_ <- getArgs
+           let root = maybe (error "Filename should end in .stl") id
+                    $ stripSuffix ".stl" filename
+           stl <- Data.STL.parse filename
+           slices <- slice settings (stlFacets stl)
+           --mapM_ (renderSlice root region . renderToolpath) slices
+           let gcode = slicesToGCode (ggGCodeSettings settings) slices
+           writeFile (root++".gcode") (unlines gcode)
+           return ()
+
+slice :: GGenSettings s -> [Face] -> IO [(Double, ToolPath)]
+slice settings faces =
+        do let zStep = ggSliceZStep settings
+               infill = ggInfillPattern settings
+           let (bbMin, bbMax) = facesBoundingBox faces
+               bbSize = bbMax .-. bbMin
+               P (_,_,zMin) = bbMin
+               P (_,_,zMax) = bbMax
+               region@(rMin,rMax) = (bbMin .-^ 0.2*^bbSize, bbMax .+^ 0.2*^bbSize)
+
+           let getSlice z = let plane = Plane { planeNormal=(0,0,1)
+                                              , planePoint=bbMin .+^ (0,0,1) ^* z }
+                                opolys = planeSlice plane faces
+                            in (z, opolys)
+
+           let nSlices = (zMax-zMin) / zStep
+               sliceZs = map (\i->zMin + i*zStep + sliceFudge) [0..nSlices]
+               slices = map getSlice sliceZs
+               toolpaths = zip sliceZs
+                         $ evalState (mapM (toolPath infill) slices) (igInitialState infill)
+
+           return toolpaths
+
+-- | stripSuffix a b strips the suffix a from list b
+stripSuffix :: Eq a => [a] -> [a] -> Maybe [a]
+stripSuffix a b
+        | null a            = Just b
+        | a `isSuffixOf` b  = stripSuffix (init a) (init b)
+        | otherwise         = Nothing
+
+renderSlice :: String -> Box Vec3 -> (Double, ToolPath) -> IO ()
+renderSlice rootName region@(rMin,rMax) (z,tp) =
+        do printf "Slice Z=%1.2f\n" z
+           renderRegionToSVG filename (500,500) region $
+                   do renderToolpath tp
+        where filename = printf "%s-z%1.2f.svg" rootName z
+
--- Main.hs
+++ Main.hs
@@ -1,42 +1,13 @@
-{-# LANGUAGE TupleSections, PackageImports #-}
-
-module Main(main) where
-
-import Data.VectorSpace
-import Data.AffineSpace
-import Data.Maybe (mapMaybe, maybe)
-import Data.Either (either, partitionEithers)
-import Data.List (isSuffixOf, deleteFirstsBy)
-import Text.Printf
-import System.IO
-import System.Environment (getArgs)
-import "mtl" Control.Monad.State (evalState)
-
-import Data.STL
-import GGen.Geometry
-import GGen.Geometry.Polygon
-import qualified GGen.Pretty as P
-import GGen.Render
-import Text.PrettyPrint.HughesPJ (($$), (<+>))
-import GGen.Types
-import GGen.ToolPath
-import GGen.GCode
-
-import Graphics.Rendering.Cairo
-import Control.Monad (liftM)
-
--- | When we slice directly in the plane of a face, nasty numerical things can
--- happen. While we try to handle these properly, sliceFudge is one final line
--- of defence. We shift all of the slices by this amount to ensure that we
--- avoid these problems, even with nice even sliceZSteps.
-sliceFudge = 1e-4
-
-sliceZStep = 0.4
+import GGen
 
 infillOffset = 0
-infillSpacing = 1.05
+infillSpacing = 1.5
 
-infillPattern = hexInfill infillOffset infillSpacing
+settings = GGenSettings
+        { ggSliceZStep = 0.4
+        , ggInfillPattern = hexInfill infillOffset infillSpacing
+        , ggGCodeSettings = gcodeSettings
+        }
 
 layerPostlude 1 _ = [ "F600" ]
 layerPostlude _ _ = []
@@ -68,57 +39,4 @@ gcodeSettings = GCodeSettings
 	, gcRetractRate = 200 -- millimeter/min
 	}
 
--- | When we slice exactly in the plane of a face, nasty things can happen with
--- numerical error. While we try to handle these properly, sliceFudge is one
--- final line of defence. We shift all of the slices by this amount to ensure
--- that we avoid these problems, even with nice even sliceZSteps.
-sliceFudge = 1e-6
-
-main = do filename:_ <- getArgs
-          let root = maybe (error "Filename should end in .stl") id
-                   $ stripSuffix ".stl" filename
-          stl <- Data.STL.parse filename
-          let faces = stlFacets stl
-          let (bbMin, bbMax) = facesBoundingBox $ stlFacets stl
-              bbSize = bbMax .-. bbMin
-              P (_,_,zMin) = bbMin
-              P (_,_,zMax) = bbMax
-              region@(rMin,rMax) = (bbMin .-^ 0.2*^bbSize, bbMax .+^ 0.2*^bbSize)
-
-          print $ P.text "Bounding Box" <+> P.point bbMin <+> P.text "to" <+> P.point bbMax
-          --print $ P.vcat $ map (\f->P.face f <+> P.text "normal:" <+> (P.vec $ faceNormal f)) faces
-          
-          let getSlice z = let plane = Plane { planeNormal=(0,0,1)
-                                             , planePoint=bbMin .+^ (0,0,1) ^* z }
-                               opolys = planeSlice plane faces
-                           in (z, opolys)
-
-          let nSlices = (zMax-zMin) / sliceZStep
-              sliceZs = map (\i->zMin + i*sliceZStep + sliceFudge) [0..nSlices]
-              slices = map getSlice sliceZs
-              toolpaths = zip sliceZs
-	                $ evalState (mapM (toolPath infillPattern) slices) (igInitialState infillPattern)
-
-          putStrLn "Rendering Slices..."
-          mapM_ (doToolPath root region) toolpaths
-
-          putStrLn "Generating GCode..."
-          let gCode = slicesToGCode gcodeSettings toolpaths
-          writeFile (root++".gcode") $ unlines gCode
-
-          return()
-
--- | stripSuffix a b strips the suffix a from list b
-stripSuffix :: Eq a => [a] -> [a] -> Maybe [a]
-stripSuffix a b
-        | null a            = Just b
-        | a `isSuffixOf` b  = stripSuffix (init a) (init b)
-        | otherwise         = Nothing
-
-doToolPath :: String -> Box Vec3 -> (Double, ToolPath) -> IO ()
-doToolPath rootName region@(rMin,rMax) (z,tp) =
-        do printf "Slice Z=%1.2f\n" z
-           renderRegionToSVG filename (500,500) region $
-                   do renderToolpath tp
-        where filename = printf "%s-z%1.2f.svg" rootName z
-
+main = ggenMain settings
